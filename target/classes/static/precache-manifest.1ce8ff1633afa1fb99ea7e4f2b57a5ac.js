a9a38104793cb8b031511e75cf5d29f5b9057a3b
Raimundo82 <you@example.com>
2021-02-23 20:04:41 +0000

    update project files

---
+++ target/classes/static/precache-manifest.1ce8ff1633afa1fb99ea7e4f2b57a5ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1a7f7d40c23482a1332",
+    "url": "/static/css/main.9369dfde.chunk.css"
+  },
+  {
+    "revision": "d1a7f7d40c23482a1332",
+    "url": "/static/js/main.fa8db7d6.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "a07b4b5d8f208cd0f8a6",
+    "url": "/static/css/2.c9d9ec70.chunk.css"
+  },
+  {
+    "revision": "a07b4b5d8f208cd0f8a6",
+    "url": "/static/js/2.c116b27a.chunk.js"
+  },
+  {
+    "revision": "7cf7c493b0434a8ecccea24de5c50449",
+    "url": "/index.html"
+  }
+];
